8a694d8ee18fd794966cce36ef87b6cdceb057c9
Wan Chang Lai <wanchang.lai@live.com>
2022-11-24 13:49:04 +0800

    Make the Cart Button Available

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,17 @@
 import classes from './CartButton.module.css';
+import { useDispatch } from 'react-redux';
+import { uiActions } from '../../store/ui-slice';
 
 const CartButton = (props) => {
+
+  const dispatch = useDispatch();
+
+  const toggleCartHanldler = () =>{
+    dispatch(uiActions.toggle());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHanldler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
